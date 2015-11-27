69fe85912e4b20a38e629cd314241448c98f8217
lucaspasqualinivota <lpasqualini99@gmail.com>
2015-11-27 20:46:21 -0200

    Update Application.java

--- src/main/java/br/edu/unidavi/restapp/Application.java
+++ src/main/java/br/edu/unidavi/restapp/Application.java
@@ -1,21 +1,32 @@
 package br.edu.unidavi.restapp;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
-@SpringBootApplication
+/**
+ * @author Michael J. Simons, 2014-02-08
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableScheduling
+@ComponentScan
 public class Application extends SpringBootServletInitializer {
-
+    
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        // Customize the application or call application.sources(...) to add sources
-        // Since our example is itself a @Configuration class we actually don't
-        // need to override this method.
-        return application;
+	return application.sources(Application.class);
     }
 
+    public static void main(String... args) {
+	System.setProperty("spring.profiles.default", System.getProperty("spring.profiles.default", "dev"));
+	final ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+    }
 }

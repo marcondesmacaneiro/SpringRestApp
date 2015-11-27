6a18de40657383bba31a56c7df9f6a9caaa1a652
cleberschmidt <cleber_blacks7@hotmail.com>
2015-11-27 20:20:10 -0200

    Update Application.java

--- src/main/java/br/edu/unidavi/restapp/Application.java
+++ src/main/java/br/edu/unidavi/restapp/Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

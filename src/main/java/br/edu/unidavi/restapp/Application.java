105b412fd75136d4b6e0110ec984e103b9d3a3bb
cleberschmidt <cleber_blacks7@hotmail.com>
2015-11-27 20:27:57 -0200

    Update Application.java

--- src/main/java/br/edu/unidavi/restapp/Application.java
+++ src/main/java/br/edu/unidavi/restapp/Application.java
@@ -1,24 +1,15 @@
 package br.edu.unidavi.restapp;
 
- import org.springframework.boot.SpringApplication;
- import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
- import org.springframework.boot.builder.SpringApplicationBuilder;
- import org.springframework.boot.web.SpringBootServletInitializer;
- import org.springframework.context.annotation.ComponentScan;
- import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     
-    @Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
-
-    //@Override
-    //protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    //    return application.sources(Application.class);
-    //}
+@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

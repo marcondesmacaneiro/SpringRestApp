2fd498b2e6c29d8b8d79154fe596b3f92233a70c
cleberschmidt <cleber_blacks7@hotmail.com>
2015-11-27 20:37:27 -0200

    Update Application.java

--- src/main/java/br/edu/unidavi/restapp/Application.java
+++ src/main/java/br/edu/unidavi/restapp/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.SpringBootServletInitializer;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
     }

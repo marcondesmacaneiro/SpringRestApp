b9adf414ab6e388297b44aa66daf0b398059de7c
cleberschmidt <cleber_blacks7@hotmail.com>
2015-11-27 20:22:18 -0200

    Update Application.java

--- src/main/java/br/edu/unidavi/restapp/Application.java
+++ src/main/java/br/edu/unidavi/restapp/Application.java
@@ -2,6 +2,7 @@ package br.edu.unidavi.restapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
